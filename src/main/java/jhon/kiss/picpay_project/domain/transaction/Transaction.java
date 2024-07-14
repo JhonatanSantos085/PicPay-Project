945d4df5064153b97ef18f19dbafed460c11d06d
Jhonatan Santos <jhonatansantos085@gmail.com>
2024-07-14 00:43:48 -0300

    Adcionei classe de transação

--- src/main/java/jhon/kiss/picpay_project/domain/transaction/Transaction.java
+++ src/main/java/jhon/kiss/picpay_project/domain/transaction/Transaction.java
@@ -1,10 +1,37 @@
-package jhon.kiss.picpay_project.domain;
+package jhon.kiss.picpay_project.domain.transaction;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity(name = "transactions")
 @Table(name = "transactions")
+@Setter
+@Getter
+@EqualsAndHashCode(of = "id")
+
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "payer-id")
+    private User payer;
+
+    @ManyToOne
+    @JoinColumn(name = "payee_id")
+    private User payee;
+
+    private LocalDateTime transactionTime
+
+
 }
